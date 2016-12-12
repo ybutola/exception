27711d921dad99130738014f80b040fc4d99c4f2
ybutola <yogendra_s_butola@optum.com>
2016-12-12 16:31:26 -0600

    added application and setter getters.

--- src/main/java/com/log/exception/vo/LogExcpetion.java
+++ src/main/java/com/log/exception/vo/LogExcpetion.java
@@ -1,22 +1,88 @@
 package com.log.exception.vo;
 
-import java.util.Date;
-
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import java.util.Date;
 
 @Entity
 public class LogExcpetion {
+    protected LogExcpetion() {
+    }
 
-	@Id
-	@GeneratedValue (strategy=GenerationType.AUTO)
-	long logID;
-	String logType; //Message, Exception
-	String message;
-	String stackTrace;
-	String cause;
-	Date logDate;
-	String application;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    long logID;
+    String logType; //Message, Exception
+    String logLevel; //warn, info, debug, error etc.
+    String message;
+    String stackTrace;
+    String cause;
+    Date logDate;
+    String application;
+
+    public long getLogID() {
+        return logID;
+    }
+
+    public void setLogID(long logID) {
+        this.logID = logID;
+    }
+
+    public String getLogType() {
+        return logType;
+    }
+
+    public void setLogType(String logType) {
+        this.logType = logType;
+    }
+
+    public String getLogLevel() {
+        return logLevel;
+    }
+
+    public void setLogLevel(String logLevel) {
+        this.logLevel = logLevel;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getStackTrace() {
+        return stackTrace;
+    }
+
+    public void setStackTrace(String stackTrace) {
+        this.stackTrace = stackTrace;
+    }
+
+    public String getCause() {
+        return cause;
+    }
+
+    public void setCause(String cause) {
+        this.cause = cause;
+    }
+
+    public Date getLogDate() {
+        return logDate;
+    }
+
+    public void setLogDate(Date logDate) {
+        this.logDate = logDate;
+    }
+
+    public String getApplication() {
+        return application;
+    }
+
+    public void setApplication(String application) {
+        this.application = application;
+    }
 }
