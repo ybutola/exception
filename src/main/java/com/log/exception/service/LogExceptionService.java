aeedde5919b8a4cfdb1235e86e8ba8e4eab55658
ybutola <yogendra_s_butola@optum.com>
2016-12-14 13:57:59 -0600

    Added some code.

--- src/main/java/com/log/exception/service/LogExceptionService.java
+++ src/main/java/com/log/exception/service/LogExceptionService.java
@@ -1,5 +1,7 @@
 package com.log.exception.service;
 
+import com.log.exception.vo.LogExcpetion;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,8 +9,14 @@ import org.springframework.web.bind.annotation.RestController;
 class LogExceptionService {
 	
 	@RequestMapping(value="/logException")
-	public void logException(String appID, Exception exception){
-
+	public void logException(@PathVariable String appID, Exception exception){
+		LogExcpetion logExcpetion = new LogExcpetion();
+		logExcpetion.setCause(exception.getCause().toString());
+		logExcpetion.setApplication(appID);
+		logExcpetion.setLogType("EX");
+		logExcpetion.setMessage(exception.getMessage());
+		logExcpetion.setLogLevel("error");
+		logExcpetion.setStackTrace(exception.getStackTrace().toString());
 	}
 	
 	@RequestMapping(value="/logMessage")
